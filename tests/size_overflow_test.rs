ddb1d08eea15fb2042338dc4ed0f2d0af93c02f6
Brian Quach <bquach88@gmail.com>
2017-08-07 01:22:37 -0400

    Add first size overflow test.

---
+++ tests/size_overflow_test.rs
@@ -0,0 +1,71 @@
+#[macro_use]
+extern crate yoga;
+
+use yoga::{Direction, Node, Point, Undefined};
+use yoga::FlexStyle::*;
+
+#[test]
+fn test_nested_overflowing_child() {
+
+	let mut root = Node::new();
+
+	style!(root,
+		Width(100 pt),
+		Height(100 pt)
+	);
+
+	let mut root_child_0 = Node::new();
+
+	root.insert_child(&mut root_child_0, 0);
+
+	let mut root_child_0_child_0 = Node::new();
+
+	style!(root_child_0_child_0, 
+		Width(200 pt), 
+		Height(200 pt)
+	);
+
+	root_child_0.insert_child(&mut root_child_0_child_0, 0);
+
+	root.calculate_layout(Undefined, Undefined, Direction::LTR);
+
+	let root_layout = root.get_layout();
+	let root_child_0_layout = root_child_0.get_layout();
+	let root_child_0_child_0_layout = root_child_0_child_0.get_layout();
+
+	assert_eq!(0.0, root_layout.left);
+	assert_eq!(0.0, root_layout.top);
+	assert_eq!(100.0, root_layout.width);
+	assert_eq!(100.0, root_layout.height);
+
+	assert_eq!(0.0, root_child_0_layout.left);
+	assert_eq!(0.0, root_child_0_layout.top);
+	assert_eq!(100.0, root_child_0_layout.width);
+	assert_eq!(200.0, root_child_0_layout.height);
+
+	assert_eq!(0.0, root_child_0_child_0_layout.left);
+	assert_eq!(0.0, root_child_0_child_0_layout.top);
+	assert_eq!(200.0, root_child_0_child_0_layout.width);
+	assert_eq!(200.0, root_child_0_child_0_layout.height);
+
+	root.calculate_layout(Undefined, Undefined, Direction::RTL);
+
+	let root_layout = root.get_layout();
+	let root_child_0_layout = root_child_0.get_layout();
+	let root_child_0_child_0_layout = root_child_0_child_0.get_layout();
+
+	assert_eq!(0.0, root_layout.left);
+	assert_eq!(0.0, root_layout.top);
+	assert_eq!(100.0, root_layout.width);
+	assert_eq!(100.0, root_layout.height);
+
+	assert_eq!(0.0, root_child_0_layout.left);
+	assert_eq!(0.0, root_child_0_layout.top);
+	assert_eq!(100.0, root_child_0_layout.width);
+	assert_eq!(200.0, root_child_0_layout.height);
+
+	assert_eq!(-100.0, root_child_0_child_0_layout.left);
+	assert_eq!(0.0, root_child_0_child_0_layout.top);
+	assert_eq!(200.0, root_child_0_child_0_layout.width);
+	assert_eq!(200.0, root_child_0_child_0_layout.height);
+}
