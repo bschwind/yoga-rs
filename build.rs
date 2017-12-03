0e13c42da18a8172ad369a6b412cbe2fbb739638
Brian Schwind <brianmschwind@gmail.com>
2017-12-03 22:50:50 +0900

    Blacklist FP_* types

--- build.rs
+++ build.rs
@@ -19,7 +19,12 @@ fn main() {
 		.rust_target(RustTarget::Stable_1_21)
 		.rustfmt_bindings(false)
 		.rustified_enum("YG.*")
-		.hide_type("max_align_t") // This fails `cargo test` so disable for now
+		.blacklist_type("max_align_t") // This fails `cargo test` so disable for now
+		.blacklist_type("FP_NAN")
+		.blacklist_type("FP_INFINITE")
+		.blacklist_type("FP_ZERO")
+		.blacklist_type("FP_SUBNORMAL")
+		.blacklist_type("FP_NORMAL")
 		.header("src/c/wrapper.h")
 		.generate()
 		.expect("Unable to generate bindings");
