e8dc2cef61795c6c2f3f4e6ff12a57ac55f18e4d
Brian Schwind <brianmschwind@gmail.com>
2018-02-19 21:22:50 +0900

    Get the build working on linux

--- build.rs
+++ build.rs
@@ -6,10 +6,12 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-	Build::new()
+	let mut builder = Build::new();
+
+	builder
 		.cpp(true)
 		// https://github.com/facebook/yoga/blob/7f44ec512e7d150b7312336ed7908ac86441b4cc/BUCK#L13
-		.flag("-std=c++11")
+		.flag("-std=c++1y")
 		// https://github.com/facebook/yoga/blob/7f44ec512e7d150b7312336ed7908ac86441b4cc/yoga_defs.bzl#L28
 		.flag("-fno-omit-frame-pointer")
 		.flag("-fexceptions")
@@ -22,10 +24,14 @@ fn main() {
 		.file("src/c/YGEnums.cpp")
 		.file("src/c/YGNode.cpp")
 		.file("src/c/YGNodePrint.cpp")
-		.file("src/c/Yoga.cpp")
-		.compile("libyoga.a");
+		.file("src/c/Yoga.cpp");
+
+	println!("Compiler is {:?}", builder.get_compiler());
+
+	builder.compile("libyoga.a");
 
 	let bindings = bindgen::Builder::default()
+		.clang_arg("-std=c++11")
 		.no_unstable_rust()
 		.no_convert_floats()
 		.enable_cxx_namespaces()
