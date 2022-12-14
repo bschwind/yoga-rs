fadb2649fcecc442a926294e61dd4f7c48b48b60
Nico Burns <nico@nicoburns.com>
2022-12-14 23:02:59 +0000

    Upgrade to yoga ba27f9d (2022-12-16)
    Full commit hash: ba27f9d1ecfa7518019845b84b035d3d4a2a6658

--- build.rs
+++ build.rs
@@ -27,15 +27,20 @@ fn main() {
 		.flag("-ffast-math")
 		// https://github.com/facebook/yoga/blob/c5f826de8306e5fbe5963f944c75add827e096c3/yoga_defs.bzl#L58-L60
 		.flag("-fPIC")
+		// Include path
+		.include("src/yoga")
 		// C++ Files
+		.file("src/yoga/yoga/event/event.cpp")
+		.file("src/yoga/yoga/internal/experiments.cpp")
+		.file("src/yoga/yoga/log.cpp")
 		.file("src/yoga/yoga/Utils.cpp")
 		.file("src/yoga/yoga/YGConfig.cpp")
 		.file("src/yoga/yoga/YGEnums.cpp")
-		.file("src/yoga/yoga/YGFloatOptional.cpp")
 		.file("src/yoga/yoga/YGLayout.cpp")
 		.file("src/yoga/yoga/YGNode.cpp")
 		.file("src/yoga/yoga/YGNodePrint.cpp")
 		.file("src/yoga/yoga/YGStyle.cpp")
+		.file("src/yoga/yoga/YGValue.cpp")
 		.file("src/yoga/yoga/Yoga.cpp")
 		.compile("libyoga.a");
 
@@ -47,9 +52,9 @@ fn main() {
         .whitelist_function("YG.*")
         .whitelist_var("YG.*")
         .layout_tests(false)
-        .rustfmt_bindings(false)
+        .rustfmt_bindings(true)
         .rustified_enum("YG.*")
-        .header("src/yoga/yoga/Yoga.h")
+        .header("src/wrapper.hpp")
         .generate()
         .expect("Unable to generate bindings");
 
